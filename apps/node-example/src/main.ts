cc4d62a139cb6083086d094ff27b2b96319da303
whalemare <nanostet@gmail.com>
2023-01-22 12:49:20 +0300

    Remove extras

--- apps/node-example/src/main.ts
+++ apps/node-example/src/main.ts
@@ -2,18 +2,6 @@ import { nestedApp } from './app/nestedApp'
 
 async function run() {
   await nestedApp()
-
-  // await pipeline(async () => {
-  //   void ticker(1000)
-
-  //   const result = await sum(1, 3)
-  //   await multiply(result, 2)
-
-  //   void tickerLargeHistory(250 * result)
-  //   void progress(100)
-
-  //   await shell('ls -a')
-  // })
 }
 
 void run()
