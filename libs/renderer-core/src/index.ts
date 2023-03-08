431b4c295c5ee11260eac2b2a9ae12ad4fd5f946
whalemare <nanostet@gmail.com>
2023-03-08 12:49:46 +0300

    🎉 Increment version 0.0.41

--- libs/renderer-core/src/index.ts
+++ libs/renderer-core/src/index.ts
@@ -1,4 +1,6 @@
 // @index(['./shared/**/*.(ts|tsx)', '!./**/*.d.(ts|tsx)'], f => `export * from '${f.path}'`)
 export * from './shared/entity/AppRender'
+export * from './shared/model/PlainConsoleRender'
+export * from './shared/model/TaskStringRenderable'
 export * from './shared/renderToConsole'
 // @endindex
