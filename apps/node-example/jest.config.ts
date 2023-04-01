714bd2efc0c6cd6b77fdd7c5ff1b93789d168a83
whalemare <nanostet@gmail.com>
2023-04-01 20:09:01 +0300

    Migrate to latest nx

--- apps/node-example/jest.config.ts
+++ apps/node-example/jest.config.ts
@@ -2,14 +2,15 @@
 export default {
   displayName: 'node-example',
   preset: '../../jest.preset.js',
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-    },
-  },
+  globals: {},
   testEnvironment: 'node',
   transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+      },
+    ],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/node-example',
